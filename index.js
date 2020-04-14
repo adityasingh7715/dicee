0a962dbeff014b0502c157372b4c045b0e7e4b92
Aditya Singh <61649469+adityasingh7715@users.noreply.github.com>
2020-04-14 18:36:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -10,7 +8,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
